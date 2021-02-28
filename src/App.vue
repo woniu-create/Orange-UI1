98fded5a3d21e6b3d20bc6e4439525ec4774f311
luyandan <2103029766@qq.com>
2021-02-28 23:28:49 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi</div>
+<Super/>
+</template>
+
+<script>
+import Super from './components/Super.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Super
+  }
+}
+</script>
