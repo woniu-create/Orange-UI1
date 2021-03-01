a4a3ac158a375dae909699ed661f34ae244f5077
luyandan <2103029766@qq.com>
2021-03-01 13:29:13 +0800

    home页面加上样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <div class="banner">
         <h1>轱辘UI</h1>
         <h2>一个很厉害的轱辘UI</h2>
-        <p class="action">
+        <p class="actions">
           <a>GitHub</a>
           <a>开始</a>
         </p>
@@ -20,5 +20,25 @@ import Topnav from '../components/Topnav.vue'
 </script>
 
 <style lang="scss" scoped>
-
+  .banner{
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+        padding: 8px 0;
+        a{
+          margin: 0 8px;
+          background: #fff;
+          display: inline-block;
+          $h:28px;
+          height: $h;
+          line-height: $h;
+          border-radius: $h/2;
+          padding: 0 8px;
+        }
+    }
+  }
 </style>
