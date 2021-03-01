1d1fa03300dfb1a745e152608fff67d322573ba9
luyandan <2103029766@qq.com>
2021-03-01 13:53:37 +0800

    确定aside的样式及位置

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,18 +3,21 @@
     <Topnav />
     <div class="content">
       <aside>
-        <li>
-          <router-link to="/doc/switch">Switch组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialog">Dialog组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs组件</router-link>
-        </li>
+        <h2>组件内容</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
+          </li>
+        </ol>
       </aside>
       <main>内容</main>
     </div>
@@ -22,11 +25,28 @@
 </template>
 
 <script>
-import Topnav from '../components/Topnav.vue'
-    export default {
-      components:{Topnav}
-    }
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
 </script>
 
 <style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
