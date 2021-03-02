907e7c5758c593fd50ef846a4fde295c2216e37c
luyandan <2103029766@qq.com>
2021-03-02 15:21:20 +0800

    实现size属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,31 @@
 <template>
-    <button class="gulu-button" :class="{[`gulu-theme-${theme}`]:theme}">
-      <slot />
-    </button>
+  <button class="gulu-button" :class="classes">
+    <slot />
+  </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
-    props:{
-      theme:{
-         type: String,
-         default:'button'
-      } 
+  props: {
+    theme: {
+      type: String,
+      default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+      };
+    });
+    return { classes };
+  },
 };
 </script>
 <style lang="scss">
@@ -47,21 +62,33 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-    &.gulu-theme-link{
+  &.gulu-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.gulu-theme-text{
+  &.gulu-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
     }
+  }
+  &.gulu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.gulu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
   }
 }
 </style>
