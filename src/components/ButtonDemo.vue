065c00d519fab89c9dffe8ef3bf338efb9956f87
luyandan <2103029766@qq.com>
2021-03-02 12:48:46 +0800

    实验attrs和props

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,20 @@
 <template>
  <div>Button 示例</div>
  <h1>示例1</h1>
- <Button>你好</Button>
+ <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small">
+   你好
+ </Button>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default {
-    components:{Button}
+    components:{Button},
+    setup(){
+        const onClick=()=>{
+            console.log('hi');
+        }
+        return {onClick}
+    }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,3 +1,28 @@
 <template>
-  <button><slot/></button>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
 </template>
+<script lang="ts">
+export default {
+    inheritAttrs:false,
+    props:{
+    size:String,
+    // disable:Boolean
+    },
+    setup(props,context){
+        // console.log({...props})
+        // console.log(context.attrs);
+        const {size, ...rest} = context.attrs;   
+        return {size,rest}
+        
+    }
+};
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
