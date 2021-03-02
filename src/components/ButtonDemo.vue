8579eb0eb2c3eec6f3dd11d0605ca368f5adde62
luyandan <2103029766@qq.com>
2021-03-02 12:15:06 +0800

    Button组件骨架基本完成

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-    Button 示例   
-    </div>
+ <div>Button 示例</div>
+ <h1>示例1</h1>
+ <Button>你好</Button>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+    components:{Button}
+}
+</script>
