e9abf8744db37dabdfbeccbad34443f9948f4663
luyandan <2103029766@qq.com>
2021-03-04 19:53:25 +0800

    代码优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
-      <div
-        class="gulu-tabs-nav-item"
-        @click="select(t)"
-        :class="{ selected: t === selected }"
-        v-for="(t, index) in titles"
-        :key="index"
-        :ref="
-          (el) => {
-            if (el) navItems[index] = el;
-          }
-        "
-      >
+      <!-- <div class="gulu-tabs-nav-item" @click="select(t)" :class="{ selected: t === selected }" v-for="(t, index) in titles" :key="index"
+        :ref=" (el) => { if (el) navItems[index] = el;}"> -->
+           <div class="gulu-tabs-nav-item" @click="select(t)" :class="{ selected: t === selected }" v-for="(t, index) in titles" :key="index"
+        :ref=" (el) => { if (t===selected) selectedItem = el;}">
         {{ t }}
       </div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
@@ -37,19 +29,21 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    // const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem=ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      console.log(result);
-      const { width } = result.getBoundingClientRect();
+    //   const divs = navItems.value;
+    //   const result = divs.filter((div) =>
+    //     div.classList.contains("selected")
+    //   )[0];
+    //   console.log(result);
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+    //   const { left: left2 } = result.getBoundingClientRect();
+     const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -78,7 +72,8 @@ export default {
       titles,
       current,
       select,
-      navItems,
+    //   navItems,
+    selectedItem,
       indicator,
       container,
     };
