d01ec8b405f8991c17f2bebfe5000fb84ff5cb96
luyandan <2103029766@qq.com>
2021-03-04 14:02:11 +0800

    遇到vue3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" :class="{selected:t===selected}" v-for="(t, index) in titles" :key="index">
+      <div
+        class="gulu-tabs-nav-item"
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
         {{ t }}
       </div>
     </div>
     <div class="gulus-tabs-content">
-      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+        {{current}}
+      <component class="gulu-tabs-content-item" :is="current" />
     </div>
   </div>
 </template>
 <script lang="ts">
+import { computed } from 'vue';
 import Tab from "./Tab.vue";
 export default {
   props: {
-      selected: {
-          type: String
-      }
+    selected: {
+      type: String,
+    },
   },
   setup(props, context) {
     const defaults = context.slots.default();
@@ -25,12 +33,25 @@ export default {
         throw new Error("Tabs 子标签必须是Tab");
       }
     });
+    const current = computed(() => {
+        console.log("重新return");
+        
+     return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     return {
       defaults,
       titles,
+      current,
+      select,
     };
   },
 };
