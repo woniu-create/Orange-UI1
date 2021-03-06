878f74ff1d972019e4a5b98c735dddf800ecd3f1
luyandan <2103029766@qq.com>
2021-03-06 18:16:28 +0800

    消除重复 2

--- src/router.ts
+++ src/router.ts
@@ -6,12 +6,10 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
-
-
 const history = createWebHashHistory()
+import {h} from 'vue'
+import Markdown from './components/Markdown.vue';
+
 export const router = createRouter({
   history: history,
   routes: [
@@ -21,9 +19,9 @@ export const router = createRouter({
      component: Doc,
      children: [
          {path: "",component:DocDemo},
-         {path: "intro",component: Intro},
-         {path: "install",component:Install},
-         {path: "get-started",component: GetStarted},
+         {path: "intro",component:h(Markdown,{path:'../markdown/intro.md',key:'1'})},
+         {path: "install",component:h(Markdown,{path:'../markdown/install.md',key:'2'})},
+         {path: "get-started",component:h(Markdown,{path:'../markdown/get-started.md',key:'3'})},
          {path: "switch",component: SwitchDemo},
          {path: "button",component: ButtonDemo},
          {path: "dialog",component: DialogDemo},
