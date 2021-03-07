c168e0c0691365b2850953ef9e82dce09390ddd9
luyandan <2103029766@qq.com>
2021-03-07 08:49:45 +0800

    现实button源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -11,7 +11,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre>{{Switch1Demo.__sourceCode}}</pre>
     </div>
   </div>
   <div class="demo">
@@ -23,7 +23,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      <pre>{{Switch2Demo.__sourceCode}}</pre>
     </div>
   </div>
 </div>
@@ -47,7 +47,9 @@ export default {
   setup() {
     const bool = ref(false)
     return {
-      bool
+      bool,
+      Switch1Demo,
+      Switch2Demo
     }
   }
 }
