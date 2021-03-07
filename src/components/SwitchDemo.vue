ed5e6a496f4ed981cb7bc0d496549372c8aa0451
luyandan <2103029766@qq.com>
2021-03-07 14:36:58 +0800

    把无用代码删除

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,13 +7,9 @@
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
 import Demo from './Demo.vue'
 import {
   ref
@@ -26,10 +22,8 @@ export default {
   setup() {
     const bool = ref(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism
     }
   }
 }
