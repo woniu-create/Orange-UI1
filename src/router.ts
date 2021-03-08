891bdc261845d2eae5d3af665d4f7ccbb7153378
luyandan <2103029766@qq.com>
2021-03-08 23:45:46 +0800

    解决build之后不能查看md文件

--- src/router.ts
+++ src/router.ts
@@ -5,10 +5,13 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 const history = createWebHashHistory()
 import {h} from 'vue'
 import Markdown from './components/Markdown.vue';
-const md = filename=>h(Markdown,{path:`../markdown/${filename}.md`,key:filename})
+const md = string=>h(Markdown,{content: string,key:string})
 
 export const router = createRouter({
   history: history,
@@ -19,9 +22,9 @@ export const router = createRouter({
      component: Doc,
      children: [
          {path: "",redirect: '/doc/intro'},
-         {path: "intro",component:md('intro')},
-         {path: "get-started",component:md('get-started')},
-         {path: "install",component:md('install')},
+         {path: "intro",component:md(intro)},
+         {path: "get-started",component:md(getStarted)},
+         {path: "install",component:md(install)},
          {path: "switch",component: SwitchDemo},
          {path: "button",component: ButtonDemo},
          {path: "dialog",component: DialogDemo},
